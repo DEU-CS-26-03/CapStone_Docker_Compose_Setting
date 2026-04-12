5fd8aef3599769ed3702a4d0f6f0d43f213d707e
JeongYeonJun <jyj36900@gmail.com>
2026-04-12 23:51:01 +0900

    chore : DTO도 같이 막기

--- spring/src/main/java/com/capstone/tryon/dto/TryonCreateRequest.java
+++ spring/src/main/java/com/capstone/tryon/dto/TryonCreateRequest.java
@@ -1,17 +1,19 @@
 package com.capstone.tryon.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TryonCreateRequest {
 
     @JsonProperty("user_image_id")
+    @NotBlank(message = "user_image_id는 필수입니다.")
     private String userImageId;
 
     @JsonProperty("garment_id")
+    @NotBlank(message = "garment_id는 필수입니다.")
     private String garmentId;
-
-    public String getUserImageId() { return userImageId; }
-    public void setUserImageId(String userImageId) { this.userImageId = userImageId; }
-    public String getGarmentId() { return garmentId; }
-    public void setGarmentId(String garmentId) { this.garmentId = garmentId; }
 }
