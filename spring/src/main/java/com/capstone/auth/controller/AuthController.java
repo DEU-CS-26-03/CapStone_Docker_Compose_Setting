9ced4e53fed1deed1c674716e73c8abd35e394a9
JeongYeonJun <jyj36900@gmail.com>
2026-04-12 19:59:30 +0900

    chore : API 로그인 회원가입 수정 + 관리할 주소 예시 추가

--- spring/src/main/java/com/capstone/auth/controller/AuthController.java
+++ spring/src/main/java/com/capstone/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.capstone.auth.dto.RegisterRequest;
 import com.capstone.auth.dto.RegisterResponse;
 import com.capstone.auth.dto.TokenResponse;
 import com.capstone.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 }
